dadab53de4096c801d61d2bd61f3f41a5074dd1a
Stephen Orchowski <steveoaz@gmail.com>
2018-02-03 15:45:03 -0700

    add script for updating arduino ide libraries

---
+++ lib/copylibstoide.sh
@@ -0,0 +1,17 @@
+mkdir -p /c/Users/steve/Documents/Arduino/libraries/LS7366
+cp ArduinoLibs/LS7366/LS7366/LS7366R.cpp /c/Users/steve/Documents/Arduino/libraries/LS7366/
+cp ArduinoLibs/LS7366/LS7366/LS7366R.h /c/Users/steve/Documents/Arduino/libraries/LS7366/
+
+mkdir -p /c/Users/steve/Documents/Arduino/libraries/MPU9250/
+cp ArduinoLibs/MPU-9250/Libraries/Arduino/src/MPU9250.cpp /c/Users/steve/Documents/Arduino/libraries/MPU9250
+cp ArduinoLibs/MPU-9250/Libraries/Arduino/src/MPU9250.h /c/Users/steve/Documents/Arduino/libraries/MPU9250
+
+mkdir -p /c/Users/steve/Documents/Arduino/libraries/quaternionFilters/
+cp ArduinoLibs/MPU-9250/Libraries/Arduino/src/quaternionFilters.cpp /c/Users/steve/Documents/Arduino/libraries/quaternionFilters
+cp ArduinoLibs/MPU-9250/Libraries/Arduino/src/quaternionFilters.h /c/Users/steve/Documents/Arduino/libraries/quaternionFilters
+
+mkdir -p /c/Users/steve/Documents/Arduino/libraries/SonarArray
+cp ArduinoLibs/SonarArray/SonarArray/SonarArray.cpp /c/Users/steve/Documents/Arduino/libraries/SonarArray/
+cp ArduinoLibs/SonarArray/SonarArray/SonarArray.h /c/Users/steve/Documents/Arduino/libraries/SonarArray/
+
+
