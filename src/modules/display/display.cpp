a47b3cec77d567462c44025168f66c9b52dde2c8
Stephen Orchowski <steveoaz@gmail.com>
2018-03-10 17:14:52 -0700

    update display program

--- src/modules/display/display.cpp
+++ src/modules/display/display.cpp
@@ -15,16 +15,20 @@ struct avc_data_t {
 	float sonar5;
 	float sonar6;
 	float sonar7;
+	unsigned int sonar_count;
 
 	float ir1;
 	float ir2;
 	float ir3;
+	unsigned int ir_count;
 
 	float velocity;
+	unsigned int velocity_count;
 	
 	float mag_x;
 	float mag_y;
 	float mag_z;
+	unsigned int mag_count;
 
 	float imu_acc_x;
 	float imu_acc_y;
@@ -32,78 +36,110 @@ struct avc_data_t {
 	float imu_ang_x;
 	float imu_ang_y;
 	float imu_ang_z;
+	unsigned int imu_count;
 
 	float latitude;
 	float longitude;
 	float altitude;
-
+	unsigned int gps_count;
 } avc_data;
 
-void sonarCallback(const sensor_msgs::Range& range_message)
+void sonarCallback(const sensor_msgs::Range::ConstPtr& range_message)
 {
-	char * frame_id = range_message.header.frame_id;
+	sonar_count += 1;
 
-	if (("/sonar1".compare(frame_id) == 0) {
-		avc_data.sonar1 = range_message.range;
-	} else if (("/sonar2".compare(frame_id) == 0) {
-		avc_data.sonar2 = range_message.range;
-	} else if (("/sonar3".compare(frame_id) == 0) {
-		avc_data.sonar3 = range_message.range;
-	} else if (("/sonar4".compare(frame_id) == 0) {
-		avc_data.sonar4 = range_message.range;
-	} else if (("/sonar5".compare(frame_id) == 0) {
-		avc_data.sonar5 = range_message.range;
-	} else if (("/sonar6".compare(frame_id) == 0) {
-		avc_data.sonar6 = range_message.range;
-	} else if (("/sonar7".compare(frame_id) == 0) {
-		avc_data.sonar7 = range_message.range;
+	const char * frame_id = range_message->header.frame_id.c_str();
+
+	if (strcmp("/sonar1", frame_id) == 0) {
+		avc_data.sonar1 = range_message->range;
+	} else if (strcmp("/sonar2", frame_id) == 0) {
+		avc_data.sonar2 = range_message->range;
+	} else if (strcmp("/sonar3", frame_id) == 0) {
+		avc_data.sonar3 = range_message->range;
+	} else if (strcmp("/sonar4", frame_id) == 0) {
+		avc_data.sonar4 = range_message->range;
+	} else if (strcmp("/sonar5", frame_id) == 0) {
+		avc_data.sonar5 = range_message->range;
+	} else if (strcmp("/sonar6", frame_id) == 0) {
+		avc_data.sonar6 = range_message->range;
+	} else if (strcmp("/sonar7", frame_id) == 0) {
+		avc_data.sonar7 = range_message->range;
 	}
 }
 
-void odometerCallback(const geometry_msgs::TwistStamped& odom_message)
+void odometerCallback(const geometry_msgs::TwistStamped::ConstPtr& odom_message)
 {
-	avc_data.velocity = odom_message.twist.linear.x;
+	velocity_count += 1;
+
+	avc_data.velocity = odom_message->twist.linear.x;
 }
 
-void imuCallback(sensor_msgs::Imu& imu_message)
+void imuCallback(const sensor_msgs::Imu::ConstPtr& imu_message)
 {
-	avc_data.imu_acc_x = imu_message.linear_acceleration.x;
-	avc_data.imu_acc_y = imu_message.linear_acceleration.y;
-	avc_data.imu_acc_z = imu_message.linear_acceleration.z;
-	avc_data.imu_ang_x = imu_message.angular_velocity.x;
-	avc_data.imu_ang_y = imu_message.angular_velocity.y;
-	avc_data.imu_ang_z = imu_message.angular_velocity.z;
+	imu_count += 1;
+
+	avc_data.imu_acc_x = imu_message->linear_acceleration.x;
+	avc_data.imu_acc_y = imu_message->linear_acceleration.y;
+	avc_data.imu_acc_z = imu_message->linear_acceleration.z;
+	avc_data.imu_ang_x = imu_message->angular_velocity.x;
+	avc_data.imu_ang_y = imu_message->angular_velocity.y;
+	avc_data.imu_ang_z = imu_message->angular_velocity.z;
 }
 
-void irCallback(const sensor_msgs::Range& range_message)
+void irCallback(const sensor_msgs::Range::ConstPtr& range_message)
 {
-	char * frame_id = range_message.header.frame_id;
+	ir_count += 1;
 
-	if (("/ir1".compare(frame_id) == 0) {
-		avc_data.ir1 = range_message.range;
-	} else if (("/ir2".compare(frame_id) == 0) {
-		avc_data.ir2 = range_message.range;
-	} else if (("/ir3".compare(frame_id) == 0) {
-		avc_data.ir3 = range_message.range;
+	const char * frame_id = range_message->header.frame_id.c_str();
+
+	if (strcmp("/ir1", frame_id) == 0) {
+		avc_data.ir1 = range_message->range;
+	} else if (strcmp("/ir2", frame_id) == 0) {
+		avc_data.ir2 = range_message->range;
+	} else if (strcmp("/ir3", frame_id) == 0) {
+		avc_data.ir3 = range_message->range;
 	}
 }
 
-void gpsCallback(sensor_msgs::NavSatFix& navsatfix_msg)
+void gpsCallback(const sensor_msgs::NavSatFix::ConstPtr& navsatfix_msg)
 {
-	avc_data.latitude = navsatfix_msg.latitude;
-	avc_data.longitude = navsatfix_msg.longitude;
-	avc_data.altitude = navsatfix_msg.altitude;
+	gps_count += 1;
+
+	avc_data.latitude = navsatfix_msg->latitude;
+	avc_data.longitude = navsatfix_msg->longitude;
+	avc_data.altitude = navsatfix_msg->altitude;
 }
 
-void magCallback(sensor_msgs::MagneticField& mag_message)
+void magCallback(const sensor_msgs::MagneticField::ConstPtr& mag_message)
 {
-	avc_data.mag_x = mag_message.magnetic_field.x;
-	avc_data.mag_y = mag_message.magnetic_field.y;
-	avc_data.mag_z = mag_message.magnetic_field.z;
+	mag_count += 1;
+
+	avc_data.mag_x = mag_message->magnetic_field.x;
+	avc_data.mag_y = mag_message->magnetic_field.y;
+	avc_data.mag_z = mag_message->magnetic_field.z;
+}
+
+void printData()
+{
+	printf("\33c\e[3J");
+	printf("sonar: #%-5d 1:%2.3f 2:%2.3f 3:%2.3f 4:%2.3f 5:%2.3f 6:%2.3f 7:%2.3f\n", avc_data.sonar_count, avc_data.sonar1, avc_data.sonar2, avc_data.sonar3, avc_data.sonar4, avc_data.sonar5, avc_data.sonar6, avc_data.sonar7);
+	printf("imu acc: #%-5d x:%3.3f y:%3.3f z:%3.3f\n",avc_data.imu_count, avc_data.imu_acc_x, avc_data.imu_acc_y, avc_data.imu_acc_z);
+	printf("imu ang: #%-5d x:%3.3f y:%3.3f z:%3.3f\n",avc_data.imu_count, avc_data.imu_ang_x, avc_data.imu_ang_y, avc_data.imu_ang_z);
+	printf("mag: #%-5d x:%3.3f y:%3.3f z:%3.3f\n", avc_data.imu_count, avc_data.mag_x, avc_data.mag_y, avc_data.mag_z);
+	printf("velocity: #%-5d - %2.3f m/s\n", avc_data.velocity_count, avc_data.velocity); 
+	printf("gps: #%-5d lat: %3.3f lon: %3.3f alt: %3.3f\n", avc_data.gps_count, avc_data.latitude, avc_data.longitude, avc_data.altitude);
+
 }
 
 int main(int argc, char **argv)
 {
+	avc_data.sonar_count = 0;
+	avc_data.ir_count = 0;
+	avc_data.mag_count = 0;
+	avc_data.imu_count = 0;
+	avc_data.gps_count = 0;
+	avc_data.velocity_count = 0;
+
 	ros::init(argc, argv, "display");
 
 	ros::NodeHandle nh;
