4d1af263fcc24a1faddb89b4789294bb3d86ed77
Stephen Orchowski <steveoaz@gmail.com>
2018-03-16 20:33:16 -0700

    Implement setup file for ros package

--- src/modules/gps/ros_package/setup.sh
+++ src/modules/gps/ros_package/setup.sh
@@ -1,20 +1,50 @@
-# Figure out ros workspace top-level directory
+#!/bin/bash
 
+ROS_WORKSPACE_HOME=""
+ROS_AVC_PROJECT_NAME="avc"
+
+# Figure out ros workspace top-level directory
+if [ "$1" == "" ]; then
+    echo "Need to pass in ros workspace parameter";
+    exit 1;
+else
+    ROS_WORKSPACE_HOME=$1
+fi
+
+echo "target ros workspace:" $ROS_WORKSPACE_HOME
 # under <ros workspace>/src/, enter "catkin_create_pkg avc"
 
-#create 'src' and 'include' directories under <ros workspace>/src/avc/
+if [ ! -d "$ROS_WORKSPACE_HOME/src" ]; then
+    echo "$ROS_WORKSPACE_HOME/src does not exist";
+    #exit 1;
+fi
+
+#create 'src' and 'include' directories under <ros workspace>/src/avc/ if necessary
+
+if [ ! -d "$ROS_WORKSPACE_HOME/src/$ROS_AVC_PROJECT_NAME" ]; then
+    echo 'avc package does not exist, creating'
+    cd $ROS_WORKSPACE_HOME/src/
+    catkin_create_pkg $ROS_AVC_PROEJCT_NAME
+    mkdir -p $ROS_WORKSPACE_HOME/src/$ROS_AVC_PROJECT_NAME/include
+    mkdir -p $ROS_WORKSPACE_HOME/src/$ROS_AVC_PROJECT_NAME/src
+    cd -
+fi
 
 #copy CMakeLists.txt and package.xml to <ros workspace/src/avc/
+cp CMakeLists.txt $ROS_WORKSPACE_HOME/src/$ROS_AVC_PROJECT_NAME/
+cp package.xml $ROS_WORKSPACE_HOME/src/$ROS_AVC_PROJECT_NAME/
 
 #copy node_read_gps.cpp to <ros_workspace>/src/avc/src
+cp ../node_read_gps.cpp $ROS_WORKSPACE_HOME/src/$ROS_AVC_PROJECT_NAME/src
 
 #copy AVC/lib/common/*.* to <ros_workspace>/src/avc/include
+cp ../../../lib/common/ $ROS_WORKSPACE_HOME/src/$ROS_AVC_PROJECT_NAME/include
 
 #in top-level ros workspace directory, enter "catkin_make"
-
-
+echo "Go to $ROS_WORKSPACE_HOME/src/, and enter 'catkin_make'"
 
 #To install new package to system,
 # TODO: check whether this should be installed in bin or lib
 # $ cd ~/catkin_ws
 # $ catkin_make install -DCMAKE_INSTALL_PREFIX=/opt/ros/kinetic  # might need sudo
+
