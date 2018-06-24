a9382f4f86e6244e922e8f5f06bafa2754b1e49a
Stephen Orchowski <steveoaz@gmail.com>
2018-06-23 17:00:37 -0700

    Add servo test sketch

---
+++ src/evaluate/servo_test/servo_test.ino
@@ -0,0 +1,50 @@
+#include <Servo.h>
+
+Servo testServo;
+
+void setup() {
+  Serial.begin(9600);
+  // put your setup code here, to run once:
+  testServo.attach(9);
+  testServo.writeMicroseconds(1500);
+  //testServo.write(90);
+
+/*
+  for(int i=0;i<90;i++) {
+    delay(50);
+    testServo.write(90+i);
+  }
+  for(int i=180;i>0;i--) {
+    delay(50);
+    testServo.write(i);
+  }
+  for(int i=0;i<=90;i++) {
+    delay(50);
+    testServo.write(i);
+  }
+  */
+ Serial.println("End of arming");
+
+  delay(5000);
+}
+
+void loop() {
+    testServo.writeMicroseconds(1600);
+    //testServo.write(105);
+    delay(3000);
+    
+    testServo.writeMicroseconds(1500);
+    //testServo.write(90);
+    delay(2000);
+    testServo.writeMicroseconds(1300);
+    //testServo.writeMicroseconds(1400);
+    //testServo.write(95);
+    delay(2000);
+    testServo.writeMicroseconds(1500);
+    delay(2000);
+    testServo.writeMicroseconds(1300);
+    delay(3000);
+    testServo.writeMicroseconds(1500);
+    delay(2000);
+    
+}
